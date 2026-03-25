f18d5351d0fedb78a7751fd7eb1053220df3a563
N Aravindan <139422186+aravindan-n-code@users.noreply.github.com>
2026-03-25 15:28:10 +0530

    Time: 0 ms (100%), Space: 19.6 MB (38.14%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -1,23 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
 class Solution:
-    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if root is None:
-            return []
-        ans = []
-        queue = deque([(root, 0)])
-
-        while(queue):
-            node, level = queue.popleft()
-            if len(ans) <= level:
-                ans.append(deque())
-
-            if level%2 == 0:
-                ans[level].append(node.val)
-            else:
-                ans[level].appendleft(node.val)
-
-            if node.left:
-                queue.append((node.left, level+1))
-            if node.right:
-                queue.append((node.right, level+1))
+    def zigzagLevelOrder(self, root):
+        result = []
         
-        return [list(d) for d in ans]
+        def dfs(node, depth):
+            if not node:
+                return
+            
+            if depth == len(result):
+                result.append([])
+            
+            if depth % 2 == 0:
+                result[depth].append(node.val)
+            else:
+                result[depth].insert(0, node.val)
+            
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+        
+        dfs(root, 0)
+        return result
